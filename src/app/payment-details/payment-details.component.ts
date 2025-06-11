5a8a0c9710ad7f140fa307080a698885f6ffbab0
Yadav-Varsha <varsha.yadav@waiin.com>
2025-06-11 17:33:00 +0530

    abc

--- src/app/payment-details/payment-details.component.ts
+++ src/app/payment-details/payment-details.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail-form.component';
+import { PaymentDetailService } from '../service/payment-detail.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-payment-details',
-  imports: [],
+  imports: [PaymentDetailFormComponent,NgFor],
   templateUrl: './payment-details.component.html',
   styles: ``
 })
 export class PaymentDetailsComponent {
-
+constructor(public service:PaymentDetailService) {}
+ngOnInit():void {
+  this.service.refreshList();   
+}
 }
---
+++ src/app/service/payment-detail.model.ts
@@ -0,0 +1,7 @@
+export class PaymentDetail {
+  paymemntDetailId:number= 0
+  cardOwnerName: string=""
+  cardNumber: string=""
+  expirationDate: string=""
+  securityCode: string=""
+}
