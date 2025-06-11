497c6ce84ed83cdb6d05982afc9a57654289eae6
Yadav-Varsha <varsha.yadav@waiin.com>
2025-06-11 11:02:01 +0530

    done

---
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-detail-form',
+  imports: [],
+  templateUrl: './payment-detail-form.component.html',
+  styles: ``
+})
+export class PaymentDetailFormComponent {
+
+}
